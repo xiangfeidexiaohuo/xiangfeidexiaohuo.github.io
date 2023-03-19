5c5467f2948ce68ae6398f65f50c2c3f3a7aad6d
jjolano <jjolano@me.com>
2023-03-19 13:39:51 -0400

    update repo

--- update.sh
+++ update.sh
@@ -1,37 +1,23 @@
 #!/bin/sh
-dpkg-scanpackages --multiversion root /dev/null > root/Packages
-cat root/Packages | xz > root/Packages.xz
-cat root/Packages | bzip2 > root/Packages.bz2
-cat root/Packages | gzip > root/Packages.gz
+dpkg-scanpackages --multiversion root > Packages
+dpkg-scanpackages --multiversion rootless >> Packages
 
-cd rootless
-dpkg-scanpackages --multiversion . /dev/null > Packages
-cat Packages | xz > Package.xz
+cat Packages | xz > Packages.xz
 cat Packages | bzip2 > Packages.bz2
 cat Packages | gzip > Packages.gz
+cat Packages | lzma > Packages.lzma
+cat Packages | zstd > Packages.zst
 
-cd ../
 apt-ftparchive\
  -o APT::FTPArchive::Release::Origin="jjolano"\
  -o APT::FTPArchive::Release::Label="jjolano"\
  -o APT::FTPArchive::Release::Suite="stable"\
  -o APT::FTPArchive::Release::Version="2.1"\
  -o APT::FTPArchive::Release::Codename="ios"\
- -o APT::FTPArchive::Release::Architectures="iphoneos-arm"\
+ -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64"\
  -o APT::FTPArchive::Release::Components="main"\
  -o APT::FTPArchive::Release::Description="personal tweak repository"\
- release root > root/Release
-
-apt-ftparchive\
- -o APT::FTPArchive::Release::Origin="jjolano (rootless)"\
- -o APT::FTPArchive::Release::Label="jjolano (rootless)"\
- -o APT::FTPArchive::Release::Suite="stable"\
- -o APT::FTPArchive::Release::Version="2.1"\
- -o APT::FTPArchive::Release::Codename="ios"\
- -o APT::FTPArchive::Release::Architectures="iphoneos-arm64"\
- -o APT::FTPArchive::Release::Components="main"\
- -o APT::FTPArchive::Release::Description="personal tweak repository"\
- release rootless > rootless/Release
+ release . > Release
 
 git add .
 git commit -m "update repo"
