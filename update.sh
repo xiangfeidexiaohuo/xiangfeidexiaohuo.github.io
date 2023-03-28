6ea75c2fb09b15745536b9ad10d38c277071de79
jjolano <jjolano@me.com>
2023-03-27 23:10:22 -0400

    update repo

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ cat Packages | xz > Packages.xz
 cat Packages | bzip2 > Packages.bz2
 cat Packages | gzip > Packages.gz
 cat Packages | lzma > Packages.lzma
-# cat Packages | zstd > Packages.zst
+cat Packages | zstd > Packages.zst
 
 apt-ftparchive\
  -o APT::FTPArchive::Release::Origin="jjolano"\
  -o APT::FTPArchive::Release::Label="jjolano"\
  -o APT::FTPArchive::Release::Suite="stable"\
- -o APT::FTPArchive::Release::Version="2.1"\
+ -o APT::FTPArchive::Release::Version="1.0"\
  -o APT::FTPArchive::Release::Codename="ios"\
  -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64"\
  -o APT::FTPArchive::Release::Components="main"\
