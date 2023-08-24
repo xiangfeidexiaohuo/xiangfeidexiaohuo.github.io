482c2c9ecb6b07b20ef12a22ce9f8820ce428ae2
xiangfeidexiaohuo <jsjson@163.com>
2023-08-24 12:23:00 +0800

    it's ok
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- update.sh
+++ update.sh
@@ -9,14 +9,14 @@ cat Packages | lzma > Packages.lzma
 cat Packages | zstd > Packages.zst
 
 apt-ftparchive\
- -o APT::FTPArchive::Release::Origin="DaoDao's Repo"\
- -o APT::FTPArchive::Release::Label="DaoDao's Repo"\
+ -o APT::FTPArchive::Release::Origin="刀刀源🇨🇳"\
+ -o APT::FTPArchive::Release::Label="刀刀源🇨🇳"\
  -o APT::FTPArchive::Release::Suite="stable"\
  -o APT::FTPArchive::Release::Version="1.0"\
  -o APT::FTPArchive::Release::Codename="ios"\
  -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64"\
  -o APT::FTPArchive::Release::Components="main"\
- -o APT::FTPArchive::Release::Description="DaoDao's tweak repository"\
+ -o APT::FTPArchive::Release::Description="刀刀个人插件源~"\
  release . > Release
 
 git add .
