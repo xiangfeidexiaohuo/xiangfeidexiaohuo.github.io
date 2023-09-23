fababb28f84c30973bef1957a256c1192f055f58
xiangfeidexiaohuo <jsjson@163.com>
2023-09-23 21:33:51 +0800

    update repo

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 dpkg-scanpackages --multiversion root > Packages
 dpkg-scanpackages --multiversion rootless >> Packages
+dpkg-scanpackages --multiversion roothide >> Packages
 
 cat Packages | xz > Packages.xz
 cat Packages | bzip2 > Packages.bz2
@@ -14,7 +15,7 @@ apt-ftparchive\
  -o APT::FTPArchive::Release::Suite="stable"\
  -o APT::FTPArchive::Release::Version="1.0"\
  -o APT::FTPArchive::Release::Codename="ios"\
- -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64"\
+ -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64 iphoneos-arm64e"\
  -o APT::FTPArchive::Release::Components="main"\
  -o APT::FTPArchive::Release::Description="刀刀个人插件源~"\
  release . > Release
