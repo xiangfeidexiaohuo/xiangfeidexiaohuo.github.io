d9f129221acc57cb90a2c492f60635fbe5282eb1
daodao <jsjson@163.com>
2026-04-09 21:55:33 +0800

    sync repo
    Signed-off-by: daodao <jsjson@163.com>

---
+++ README.md
@@ -0,0 +1,10 @@
+[![iOS](https://img.shields.io/badge/刀刀源-f13232?style=flat-square&logo=apple&logoColor=ffffff)](https://xiangfeidexiaohuo.github.io/)
+
+[![iOS](https://img.shields.io/badge/刀刀源频道-r45432?style=flat-square&logo=apple&logoColor=ffffff)](https://t.me/ae86_dao)
+
+
+#### ❤️ 支持 rootful 和 rootless、roothide，以汉化包为主，还有一些功能插件。
+
+
+
+
---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+echo "1、删除旧版Packages"
+rm Packages Packages.*
+
+echo "2、扫描重新生成、压缩Packages"
+dpkg-scanpackages --multiversion roothide >> Packages
+dpkg-scanpackages --multiversion rootless >> Packages
+dpkg-scanpackages --multiversion rootful >> Packages
+
+cat Packages | xz > Packages.xz
+cat Packages | bzip2 > Packages.bz2
+cat Packages | gzip > Packages.gz
+cat Packages | lzma > Packages.lzma
+cat Packages | zstd > Packages.zst
+
+apt-ftparchive\
+ -o APT::FTPArchive::Release::Origin="刀刀源"\
+ -o APT::FTPArchive::Release::Label="刀刀源"\
+ -o APT::FTPArchive::Release::Suite="stable"\
+ -o APT::FTPArchive::Release::Version="1.0"\
+ -o APT::FTPArchive::Release::Codename="ios"\
+ -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64 iphoneos-arm64e"\
+ -o APT::FTPArchive::Release::Components="main"\
+ -o APT::FTPArchive::Release::Description="刀刀个人插件源~"\
+ release . > Release
+
+echo "3、推送提交"
+git add .
+git commit -s -m "sync repo"
+git push
